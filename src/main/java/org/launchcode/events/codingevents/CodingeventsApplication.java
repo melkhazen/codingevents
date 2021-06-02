1bcc9b3d354797b5e7db452f813b1c0246c2ffe1
Mohamed <elkhazen1@gmail.com>
2021-06-02 14:12:22 -0500

    another commit

---
+++ src/main/java/org/launchcode/events/codingevents/CodingeventsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.events.codingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/events/codingevents/CodingeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.events.codingevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
