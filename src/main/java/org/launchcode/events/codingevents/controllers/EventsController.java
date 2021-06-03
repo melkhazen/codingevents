31dbd82cfda3d7fdac4da7fa7262efc37217196c
Mohamed <elkhazen1@gmail.com>
2021-06-02 22:08:03 -0500

    commit

---
+++ src/main/java/org/launchcode/events/codingevents/controllers/EventsController.java
@@ -0,0 +1,39 @@
+package org.launchcode.events.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventsController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String diplayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String CreateEvent(@RequestParam String eventName) {
+
+        events.add(eventName);
+        return "redirect:";
+    }
+
+}
